fd84a101b11bab9ee918d3ccb280d984d09d4a18
mekane <michael-kane@uiowa.edu>
2022-01-07 14:02:46 -0600

    inital commit, data.js test

---
+++ stats/data.js
@@ -0,0 +1,7 @@
+const yearlyData = [
+    { year: 2022, champion: 'Los Kanehos' },
+    { year: 2021, champion: 'Robert\'s Team?' },
+    { year: 2020, champion: 'Hopefully not Tim' },
+];
+
+export default yearlyData;
