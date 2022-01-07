af3be02b28620d2ae6a9c03125053423201fd6c7
mekane <4631242+mekane84@users.noreply.github.com>
2022-01-07 15:04:24 -0600

    update to use json

--- stats/data.js
+++
@@ -1,7 +0,0 @@
-const yearlyData = [
-    { year: 2022, champion: 'Los Kanehos' },
-    { year: 2021, champion: 'Robert\'s Team?' },
-    { year: 2020, champion: 'Hopefully not Tim' },
-];
-
-export default yearlyData;
